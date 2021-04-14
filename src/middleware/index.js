08b5b39975f6f937e8d2c7c805d7d596c55dff3c
Lê Minh Hào <61618982+lehao190@users.noreply.github.com>
2021-04-14 16:40:08 +0700

    Feature login signup (#12)
    * Add session
    
    * Add Session & Redis
    
    * Allow-Cross-Origin to get Set-Cookie

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,17 +1,56 @@
+const session = require('express-session')
+const redis = require('redis')
+
+const RedisStore = require('connect-redis')(session)
+const redisClient = redis.createClient()
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      secret: 'session-secret',
+      name: 'user',
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        sameSite: true,
+        httpOnly: true,
+        secure: false,
+        maxAge: 1 * 60 * 60 * 1000
+      }
+    })
+  )
 
+  // Login Route
   app.post('/login', async (req, res) => {
-    const auth = await app.service('authentication').create({
-      strategy: 'local',
-      email: req.body.email,
-      password: req.body.password
-    });
+    try {
+      const { email, password } = req.body
 
-    // console.log('auth data: ', auth)
+      // Authenticate user with Local Strategy
+      const { user, refreshToken, accessToken } = await app
+        .service('authentication')
+        .create({
+          strategy: 'local',
+          email,
+          password
+        })
 
-    res.json('Hello Dude you good, Yeah man');
-  });
-};
+      // create User's Session
+      req.session.authentication = {
+        refreshToken,
+        user
+      }
+
+      res.status(201).json(req.session.authentication)
+    } catch (error) {
+      res.status(error.code).json(error)
+    }
+  })
+
+  app.post('/aha', async (req, res) => {
+    res.json(req.session.authentication)
+  })
+}
