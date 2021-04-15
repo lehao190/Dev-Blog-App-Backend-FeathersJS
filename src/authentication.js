bcb481644251596c3bdee4599820a60b879c3915
Lê Minh Hào <61618982+lehao190@users.noreply.github.com>
2021-04-15 17:40:24 +0700

    Feature login signup (#13)
    * Add session
    
    * Add Session & Redis
    
    * Allow-Cross-Origin to get Set-Cookie
    
    * Github Oauth Login

--- src/authentication.js
+++ src/authentication.js
@@ -4,7 +4,7 @@ const {
   JWTStrategy
 } = require('@feathersjs/authentication')
 const { LocalStrategy } = require('@feathersjs/authentication-local')
-const { expressOauth } = require('@feathersjs/authentication-oauth')
+const { expressOauth, OAuthStrategy } = require('@feathersjs/authentication-oauth')
 const { NotAuthenticated } = require('@feathersjs/errors')
 
 // Custom JWT
@@ -67,7 +67,7 @@ class CustomAuthService extends AuthenticationService {
     const authUser = await this.authenticate(
       data,
       params,
-      ...this.configuration.authStrategies
+      ...authStrategies
     )
 
     if (authUser.user.password) {
@@ -116,11 +116,26 @@ class CustomAuthService extends AuthenticationService {
   }
 }
 
+class GitHubStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    return {
+      ...baseData,
+      // You can also set the display name to profile.name
+      name: profile.login,
+      // The user email address (if available)
+      email: profile.email
+    };
+  }
+}
+
 module.exports = app => {
   const authentication = new CustomAuthService(app)
 
   authentication.register('jwt', new CustomJwtStrategy())
   authentication.register('local', new LocalStrategy())
+  authentication.register('github', new GitHubStrategy())
 
   app.use('/authentication', authentication)
   app.configure(expressOauth())
